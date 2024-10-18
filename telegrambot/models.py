a46f2b47772184c3d49a0203c8548b4486fa7b4a
Nathanael <nurikwestohae@gmail.com>
2024-10-19 04:19:24 +0500

    hui7

--- telegrambot/models.py
+++ telegrambot/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    user_id = models.CharField(max_length=50, unique=True)
+    avatar_url = models.URLField()
+    completed_courses = models.ManyToManyField('Course', related_name='completed_by')
+    token_count = models.IntegerField(default=0)
+    level = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"User {self.user_id} - Tokens: {self.token_count}"
