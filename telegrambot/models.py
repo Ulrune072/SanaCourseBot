1fed67802e8578b436bd8db5d81aef13524753ed
Nathanael <nurikwestohae@gmail.com>
2024-10-19 04:33:51 +0500

    hui8

--- telegrambot/models.py
+++ telegrambot/models.py
@@ -10,3 +10,26 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"User {self.user_id} - Tokens: {self.token_count}"
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    theory = models.TextField()
+
+    def  __str__(self):
+        return self.title
+
+class Question(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='questions')
+    question_text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.question_text
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    answer_text = models.CharField(max_length=255)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        self.answer_text
